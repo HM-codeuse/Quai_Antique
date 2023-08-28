c37203f4f55cea4be7b1973362858c46c89d4047
Dsb <73273914+Umm-DSB@users.noreply.github.com>
2023-08-28 19:13:02 +0200

    configure the dontshowagain option

--- assets/intro.js
+++ assets/intro.js
@@ -3,12 +3,19 @@ import 'intro.js/introjs.css';
 //Import all of Intro JS
 import introJs from 'intro.js';
 
-introJs().setOptions({
+
+introJs()
+.setOption("dontShowAgain", true)
+.setOption("nextLabel", " > ")
+.setOption("prevLabel", " < ")
+.setOption("dontShowAgainLabel", "Ne plus afficher")
+.setOption("doneLabel", "Terminé")
+.setOptions({
     steps: [{
       intro: "Bienvenue sur le site Quai Antique! Etes-vous prêts à embarquer pour une aventure gustative?"
     }, {
       element: document.querySelector('.menu-info'),
-      intro: "Vous êtes ici sur le menu du site, vous pourrez y retrouver toutes les informations pratiques suivantes.",
+      intro: "Vous êtes ici sur le menu du site, vous pourrez y retrouver toutes les informations, vous inscrire mais aussi réserver.",
     },
         {
         element: document.querySelector('.stars-info'),
@@ -20,19 +27,7 @@ introJs().setOptions({
         },
         {
             element: document.querySelector('.openhours-info'),
-      intro: "Vérifiez si nous sommes ouverts ici!."
-        },
-        {
-            element: document.querySelector('.reservation-info'),
-      intro: "Il ne vous reste plus qu'à réserver!"
-        },
-        {
-            element: document.querySelector('.registration-info'),
-      intro: "Mais avant inscrivez-vous..."
-        },
-        {
-            element: document.querySelector('.connexion-info'),
-      intro: "ou connectez-vous!A très vite!"
+      intro: "Vérifiez si nous sommes ouverts ici et venez nous rencontrer.Vous ne serez pas déçus"
         }]
   }).start();
 
